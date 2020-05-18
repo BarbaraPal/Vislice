fbd2d70fac0f38d66bc134cff793a88fd4d37ece
BarbaraPal <pal.barbi@gmail.com>
2020-05-18 19:03:23 +0200

    bottle in ostalo

---
+++ vislice.py
@@ -0,0 +1,26 @@
+import bottle
+
+import model
+
+vislice = model.Vislice()
+
+@bottle.get('/')
+def index():
+    return bottle.template('./views/index.tpl')
+
+
+@bottle.get('igra/<id_igre:int>')
+def pokazi_igro(id_igre):
+    igra, poskus = vislice.igre[id_igre]
+
+    return bottle.template('./views/igra.tpl',
+                    igra=igra, poskus=poskus)
+
+
+
+
+
+
+
+bottle.run(reloader=True, debug=True)
+
